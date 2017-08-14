204ed5b0d10d21c2bc4057debf3d4985ba60b038
kadodds <kadodds@microsoft.com>
2017-08-14 13:50:38 -0700

    simple download of exe file in place, OS system recognition in place

--- CertificateDistribution/Controllers/HomeController.cs
+++ CertificateDistribution/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,8 +12,36 @@ namespace CertificateDistribution.Controllers
         public ActionResult Index()
         {
             ViewBag.OS = Environment.OSVersion.Platform;
+            var platform = Request.UserAgent;
+            ViewBag.Platform = platform;
 
             return View();
+        }
+
+        public ActionResult Download()
+        {
+            string fileName = "";
+            string fileType = "";
+
+            // Todo: check what OS the user is operating on
+            var platform = Request.UserAgent;
+            if (platform.Contains("Windows"))
+            {
+                fileName = "install_certificate.exe";
+                fileType = "application/octet-stream";
+            }
+            else if (platform.Contains("Mac OS"))
+            {
+                fileName = "install_certificate.";
+                fileType = "";
+            }
+            else if (platform.Contains("Linux"))
+            {
+                // show error, unsupported os
+            }
+
+            string fullPath = Path.Combine(Server.MapPath("~/Test_Content"), fileName);
+            return File(fullPath, fileType, fileName);
         }
         
     }
